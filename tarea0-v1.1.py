7176d5b5f20b242097348fb2bac4d3f240f04b17
Diego5714 <dsotocaneo@gmail.com>
2023-08-24 20:29:04 -0400

    Versión final de la tarea #0

--- tarea0-v1.1.py
+++ tarea0-v1.1.py
@@ -13,32 +13,60 @@ class Controller(pyglet.window.Window):
     def __init__(self, ancho, alto, titulo):
         super().__init__(ancho, alto, resizable = True, caption = titulo)
         self.set_minimum_size(200,200)
-
-    def update(self, dt):
+        self.ancho = ANCHO
+        self.alto = ALTO
         
+    def update(self, dt):
         pass
 
 #Creamos un controlador (ventana)
 ventana = Controller(ancho = ANCHO, alto = ALTO, titulo = "Tarea #0")
+label = pyglet.text.Label("Vista desde la esquina de abajo",font_name = 'Roboto',font_size = 16, x = ventana.ancho / 2 - 160, y = ventana.alto - 40)
 
 #Procedemos a cargar los shaders y crear el pipeline
-with open(Path(os.path.dirname(__file__)) / "auxiliares/shaders/basic.vert") as f:
-    VertexFuente = f.read()
+VertexFuente = """
+#version 330
 
-with open(Path(os.path.dirname(__file__)) / "auxiliares/shaders/basic.frag") as f:
-    FragmentFuente = f.read()
+in vec2 position;
+in vec3 color;
+in float intensity;
 
-#Creamos el pipeline:
+out vec3 fragColor;
+out float fragIntensity; 
+
+void main()
+{
+    fragColor = color;   
+    fragIntensity = intensity;  
+    gl_Position = vec4(position, 0.0f, 1.0f); 
+}
+"""
+
+
+FragmentFuente = """
+#version 330
+
+in vec3 fragColor;
+in float fragIntensity;
+out vec4 outColor;
+
+void main()
+{
+    outColor = fragIntensity * vec4(fragColor, 1.0f);
+}
+"""
+
+#Creamos el pipeline con los shaders importados
 VertexShader = pyglet.graphics.shader.Shader(VertexFuente,'vertex')
 FragmentShader = pyglet.graphics.shader.Shader(FragmentFuente, 'fragment')
 
 pipeline1 = pyglet.graphics.shader.ShaderProgram(VertexShader, FragmentShader)
 
 #Ahora definimos nuestros vertexes:
-A = [-0.75, -0.75]
+A = [-0.65, -0.7]
 B = [0.0, 0.8]
-C = [0.25, -0.25]
-D = [0.7, -0.33]
+C = [0.25, -0.4]
+D = [0.75, -0.5]
 
 Rojo = [1.0, 0.0, 0.0]
 Azul = [0.0, 0.0, 1.0]
@@ -57,24 +85,24 @@ T3 = np.array(A + C + D, dtype = np.float32)
 C3 = np.array(Amarillo + Amarillo + Amarillo, dtype = np.float32)
 
 
-#Procedemos a transformar nuestros vertexes a una vertex list indexada
+#Procedemos a transformar nuestros vertexes a una vertex list almacenada en la GPU
 triangulo1 = pipeline1.vertex_list(3, GL.GL_TRIANGLES)
 
 triangulo1.position = T1
 triangulo1.color = C1
-triangulo1.intensity = np.array([1.0, 1.0, 1.0], dtype = np.float32)
+triangulo1.intensity = np.array([1.0, 1.0, 0.8], dtype = np.float32)
 
 triangulo2 = pipeline1.vertex_list(3, GL.GL_TRIANGLES)
 
 triangulo2.position = T2
 triangulo2.color = C2
-triangulo2.intensity = np.array([1.0, 1.0, 1.0], dtype = np.float32)
+triangulo2.intensity = np.array([0.6, 0.6, 0.15], dtype = np.float32)
 
 triangulo3 = pipeline1.vertex_list(3, GL.GL_TRIANGLES)
 
 triangulo3.position = T3
 triangulo3.color = C3
-triangulo3.intensity = np.array([1.0, 1.0, 1.0], dtype = np.float32)
+triangulo3.intensity = np.array([0.3, 0.7, 0.2], dtype = np.float32)
 
 @ventana.event
 def on_draw():  #Procedemos a dibujar mediante triangulos en el pipeline
@@ -84,6 +112,7 @@ def on_draw():  #Procedemos a dibujar mediante triangulos en el pipeline
     triangulo1.draw(GL.GL_TRIANGLES)
     triangulo2.draw(GL.GL_TRIANGLES)
     triangulo3.draw(GL.GL_TRIANGLES)
+    label.draw()
 
 pyglet.clock.schedule_interval(ventana.update, 1/60)
 pyglet.app.run()    
