6b6aa128de767fa694685b6b49003e72e7bb74f6
diego5714 <diego.soto@ing.uchile.cl>
2023-11-03 15:28:36 -0300

    4ta iteracion de la tarea #2: Camara funcionando, primera version entregable

--- Tarea-2/tarea2.py
+++ Tarea-2/tarea2.py
@@ -22,6 +22,9 @@ from auxiliares.utils.helpers import init_axis, init_pipeline, mesh_from_file, g
 ANCHO = 1280
 ALTO = 800
 
+#Definimos cool down para la pulsacion de teclas:
+Max_CD = 0.5
+
 class Ventana(pyglet.window.Window):
     def __init__(self, ancho, alto, titulo):
         super().__init__(ancho, alto, resizable = True, caption = titulo)
@@ -34,13 +37,13 @@ class Ventana(pyglet.window.Window):
                                 "seleccion": 0, 
                                 "Key_Cool_Down": 0,
                                 "transicion": False, 
-                                "Offset_Inicial": 0,
-                                "Offset_Final": np.array([0, 0, 3.263], dtype=np.float32),
-                                "Offset_Actual": np.array([0, 0, 3.263], dtype=np.float32),
-                                "Vista_Inicial": 0,
-                                "Vista_Final": 0,
-                                "Vista_Actual": 0}
-                                 #valores por defecto de la posicion del centro de la camara, y a donde miramos.
+                                "Offset_Inicial": None,
+                                "Offset_Final": np.array([0, 0, 15], dtype=np.float32),
+                                "Offset_Actual": np.array([0, 0, 15], dtype=np.float32),
+                                "Parametro_Vista_Final": 0,
+                                "Parametro_Vista_Actual": 0
+                            }
+                                 #valores por defecto de la posicion del centro de la camera, y a donde miramos.
         self.init()
 
     def init(self):
@@ -50,8 +53,8 @@ class Ventana(pyglet.window.Window):
     def is_key_pressed(self, key):
         return self.key_handler[key]
 
-class CamaraOrbital(Camera):
-    def __init__(self, distancia, tipo = "perspectiva", ancho = ANCHO, alto = ALTO, center_offset = np.array([0, 0, 0], dtype=np.float32), angle_offset = 0):
+class cameraOrbital(Camera):
+    def __init__(self, distancia, tipo = "perspective", ancho = ANCHO, alto = ALTO, center_offset = np.array([0, 0, 0], dtype=np.float32), angle_offset = 0):
         super().__init__(tipo, ancho, alto)
         self.distancia = distancia
         self.center_offset = center_offset
@@ -114,17 +117,17 @@ if __name__ == "__main__":
         get_path("auxiliares/shaders/color_mesh_lit.vert"),
         get_path("auxiliares/shaders/color_mesh_lit.frag"))
 
-    
+    '''
     Ventana.program_state["camera"] = FreeCamera([2.5, 2.5, 2.5], "perspective")
     Ventana.program_state["camera"].yaw = -3* np.pi/ 4
     Ventana.program_state["camera"].pitch = -np.pi / 4
+    '''
 
-    '''
-    Ventana.program_state["camera"] = CamaraOrbital(8, "perspective", ancho = Ventana.ancho, alto = Ventana.alto)
+    Ventana.program_state["camera"] = cameraOrbital(8, ancho = Ventana.ancho, alto = Ventana.alto)
     Ventana.program_state["camera"].theta = np.pi / 2.7
-    Ventana.program_state["camera"].center_offset = np.array([0, 0, 0])
-    Ventana.program_state["camera"].focus = np.array([0, 0, 0])
-    '''
+    Ventana.program_state["camera"].center_offset = np.array([0, 0, 15])
+    Ventana.program_state["camera"].focus = np.array([0, 0, 15])
+    
 
     #Importamos el vehiculo
 
@@ -151,11 +154,12 @@ if __name__ == "__main__":
     
     Garaje.add_node("Luz_global",
                     pipeline = Color_Mesh_Lit_Pipeline,
-                    rotation = [0, 0, np.pi/2],
+                    rotation = np.array([0, 0, np.pi/2]),
                     light = DirectionalLight(
-                            ambient = [0.3, 0.2, 0.2],
-                            diffuse = [0.3, 0.2, 0.2],
-                            specular = [0.3, 0.2, 0.2]),
+                            ambient = np.array([0.3, 0.2, 0.2]),
+                            diffuse = np.array([0.3, 0.2, 0.2]),
+                            specular = np.array([0.3, 0.2, 0.2])
+                            ),
                     )
     
 
@@ -164,9 +168,10 @@ if __name__ == "__main__":
                     pipeline = Color_Mesh_Lit_Pipeline,
                     transform = tr.translate(0, 6, 0) @ tr.scale(25, 6, 25),
                     material = Material(
-                                ambient = [0.2, 0.2, 0.2],
-                                diffuse = [0.6, 0.5, 0.5],
-                                specular = [0.6, 0.5, 0.5]),
+                                ambient = np.array([0.2, 0.2, 0.2]),
+                                diffuse = np.array([0.6, 0.5, 0.5]),
+                                specular = np.array([0.6, 0.5, 0.5])
+                                ),
                     cull_face = False)
 
     ###############################################################################################################################################################
@@ -178,15 +183,15 @@ if __name__ == "__main__":
 
     Garaje.add_node("Marcador_1",
                     attach_to = "Plataforma_1",
-                    mesh = Mesh_marcador,
+                    #mesh = Mesh_marcador,
                     pipeline = Color_Mesh_Lit_Pipeline,
-                    position = [0, 5, 2],
+                    position = np.array([0, 5, 2]),
                     material = Material(
-                                ambient = [1, 1, 1],
-                                diffuse = [1, 1, 1],
-                                specular = [1, 1, 1],
+                                ambient = np.array([1, 1, 1]),
+                                diffuse = np.array([1, 1, 1]),
+                                specular = np.array([1, 1, 1]),
                                 shininess = 5
-                                )
+                                ),
                     )
 
     
@@ -282,6 +287,8 @@ if __name__ == "__main__":
                             constant = 1,
                             linear = 0.09,
                             quadratic = 0.032,
+                            outerCutOff = 0.92,
+                            cutOff = 0.99
                     )
                     )
 
@@ -318,7 +325,7 @@ if __name__ == "__main__":
 
     Garaje.add_node("Marcador_2",
                     attach_to = "Plataforma_2",
-                    mesh = Mesh_marcador,
+                    #mesh = Mesh_marcador,
                     pipeline = Color_Mesh_Lit_Pipeline,
                     position = [0, 5, 2],
                     material = Material(
@@ -422,6 +429,8 @@ if __name__ == "__main__":
                             constant = 1,
                             linear = 0.09,
                             quadratic = 0.032,
+                            outerCutOff = 0.92,
+                            cutOff = 0.99
                     )
                     )
 
@@ -458,7 +467,7 @@ if __name__ == "__main__":
 
     Garaje.add_node("Marcador_3",
                     attach_to = "Plataforma_3",
-                    mesh = Mesh_marcador,
+                    #mesh = Mesh_marcador,
                     pipeline = Color_Mesh_Lit_Pipeline,
                     position = [0, 5, 2],
                     material = Material(
@@ -562,6 +571,8 @@ if __name__ == "__main__":
                             constant = 1,
                             linear = 0.09,
                             quadratic = 0.032,
+                            outerCutOff = 0.92,
+                            cutOff = 0.99
                     )
                     )
 
@@ -582,47 +593,143 @@ if __name__ == "__main__":
                     mesh = Mesh_Rines,
                     pipeline = Color_Mesh_Lit_Pipeline,
                     material = Material(
-                                ambient = [0.1, 0.1, 0.1],
-                                diffuse = [0.776, 0.824, 0.831],
-                                specular = [0.876, 0.924, 0.931],
+                                ambient = np.array([0.1, 0.1, 0.1]),
+                                diffuse = np.array([0.776, 0.824, 0.831]),
+                                specular = np.array([0.876, 0.924, 0.931]),
                                 shininess = 128
                     )
                     )
 
 ###################################################################################################################################################################
 #Logica de actualizacion:
+    def Circunferencia(parametro):
+        x = 15 * np.cos(parametro + (np.pi / 2))
+        y = 0
+        z = 15 * np.sin(parametro + (np.pi / 2))
+
+        return np.array([x,y,z])
+        
+    
+    def Transicion_suave(dt, offset_inicial, offset_final, offset_actual):
+        signo_offset_x = np.sign(offset_final[0] - offset_actual[0])
+        signo_offset_z = np.sign(offset_final[2] - offset_actual[2])
+    
+        if signo_offset_x > 0:
+            if signo_offset_z != 0:
+                X_offset = np.array([offset_inicial[0], offset_final[0]], dtype = np.float32)
+                Y_offset = np.array([offset_inicial[2], offset_final[2]], dtype = np.float32)
+        
+                offset_actual[0] += dt * 4.5
+                offset_actual[2] = np.interp(offset_actual[0], X_offset, Y_offset)
+            else:
+                offset_actual[0] += dt * 9
+
+        if signo_offset_x < 0:
+            if signo_offset_z != 0:
+                X_offset = np.array([offset_final[0], offset_inicial[0]], dtype = np.float32)
+                Y_offset = np.array([offset_final[2], offset_inicial[2]], dtype = np.float32)
+        
+                offset_actual[0] -= dt * 4.5
+                offset_actual[2] = np.interp(offset_actual[0], X_offset, Y_offset)
+            else:
+                offset_actual[0] -= dt * 9
+        
+
+        return offset_actual
+    
+    def Elevacion_Camara():
+        pass
     
     def update(dt):
         camera = Ventana.program_state["camera"]
+        Ventana.program_state["Key_Cool_Down"] += dt
+
+        if Ventana.program_state["Key_Cool_Down"] >= 10.0:
+            Ventana.program_state["Key_Cool_Down"] = 0.01 #Para que el numero no se vaya a la cresta xD
+
+        if not Ventana.program_state["transicion"]:
+            coseno = abs(np.cos(camera.phi))  #Por mejorar
+
+            if coseno < 0.75:
+                coseno = 0.75
+
+            camera.distancia = 7.75 * coseno
+    
+            camera.phi += dt / 2
+
+            if camera.phi >= np.pi * 2:
+                camera.phi = 0
+
+        if Ventana.is_key_pressed(pyglet.window.key.SPACE) and Ventana.program_state["Key_Cool_Down"] >= 0.5 and not Ventana.program_state["transicion"]:
         
-        camera = Ventana.program_state["camera"]
-        if Ventana.is_key_pressed(pyglet.window.key.A):
-            camera.position -= camera.right * dt
-        if Ventana.is_key_pressed(pyglet.window.key.D):
-            camera.position += camera.right * dt
-        if Ventana.is_key_pressed(pyglet.window.key.W):
-            camera.position += camera.forward * dt
-        if Ventana.is_key_pressed(pyglet.window.key.S):
-            camera.position -= camera.forward * dt
-        if Ventana.is_key_pressed(pyglet.window.key.Q):
-            camera.position[1] -= dt
-        if Ventana.is_key_pressed(pyglet.window.key.E):
-            camera.position[1] += dt
-        if Ventana.is_key_pressed(pyglet.window.key._1):
-            camera.type = "perspective"
-        if Ventana.is_key_pressed(pyglet.window.key._2):
-            camera.type = "orthographic"
+            if Ventana.program_state["seleccion"] == 0:
+                Ventana.program_state["Offset_Inicial"] = np.array([0, 0, 15])
+                Ventana.program_state["Offset_Final"] = np.array([-12.99, 0, -7.5])
 
-
-
+                Ventana.program_state["Parametro_Vista_Final"] = 2 / 3
+                
+                camera.angle_offset = -2 * np.pi / 3
         
-        '''
-        camera.phi += dt / 2
+            elif Ventana.program_state["seleccion"] == 1:
+                Ventana.program_state["Offset_Inicial"] = np.array([-12.99, 0, -7.5])
+                Ventana.program_state["Offset_Final"] = np.array([12.99, 0, -7.5])
 
-        if camera.phi >= np.pi * 2:
-            camera.phi = 0
-        '''
+                Ventana.program_state["Parametro_Vista_Final"] = 4 / 3
+                
+                camera.angle_offset = -4 * np.pi / 3
 
+            else:
+                Ventana.program_state["Offset_Inicial"] = np.array([12.99, 0, -7.5])
+                Ventana.program_state["Offset_Final"] = np.array([0, 0, 15])
+
+                Ventana.program_state["Parametro_Vista_Final"] = 2
+                
+                camera.angle_offset = 0
+
+            Ventana.program_state["seleccion"] += 1
+
+            if Ventana.program_state["seleccion"] > 2:
+                Ventana.program_state["seleccion"] = 0
+
+            camera.focus = Ventana.program_state["Offset_Final"]
+            #camera.phi = 3 * np.pi / 4
+
+            Ventana.program_state["Key_Cool_Down"] = 0
+
+        #################################################################################
+
+        if np.sqrt(np.square(Ventana.program_state["Offset_Final"][0] - Ventana.program_state["Offset_Actual"][0]) + np.square(Ventana.program_state["Offset_Final"][2] - Ventana.program_state["Offset_Actual"][2])) > 0.1:
+        #Ejecutamos si la distancia entre el offset actual y el final es mayor a 0.1 (Es decir, debemos desplazarnos)
+
+            #Logica para mover suavemente el centro de la camara
+            Ventana.program_state["transicion"] = True
+            Ventana.program_state["Offset_Actual"] = Transicion_suave(dt, Ventana.program_state["Offset_Inicial"], 
+                                                                        Ventana.program_state["Offset_Final"], 
+                                                                        Ventana.program_state["Offset_Actual"],
+                                                                    )
+            
+            camera.center_offset = Ventana.program_state["Offset_Actual"]
+
+            #Logica para voltear la camara de manera suave
+            delta_parametro = Ventana.program_state["Parametro_Vista_Final"] - Ventana.program_state["Parametro_Vista_Actual"]
+
+            if delta_parametro >= 0.01:
+                Ventana.program_state["Parametro_Vista_Actual"] += dt / 4
+
+            else:
+                camera.focus = Circunferencia(Ventana.program_state["Parametro_Vista_Final"] * np.pi)
+
+            camera.focus = Circunferencia(Ventana.program_state["Parametro_Vista_Actual"] * np.pi)
+
+
+        else:
+            Ventana.program_state["transicion"] = False
+            camera.center_offset = Ventana.program_state["Offset_Final"]
+
+            if Ventana.program_state["seleccion"] == 0:
+                Ventana.program_state["Parametro_Vista_Actual"] = 0
+
+        print(Ventana.program_state["Parametro_Vista_Actual"])
         camera.update()
 
     @Ventana.event
